029f2eceec0313c4da538606cc4614274196b50f
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-11-17 09:21:05 +0000

    18/11/23 DC Question

---
+++ Daily Challenge/Update the Time.py
@@ -0,0 +1,47 @@
+"""
+
+Update the Time
+
+The program must accept a time T (in 24hr format HH:MM) and an integer X as the input. The program must update the time T
+by adding X minutes to it. Then the program must print the updated time T in the same 24hr format as the output.
+
+Boundary Condition(s):
+0 <= HH <= 23
+O <= MM <= 60
+1 <= X <= 10^8
+
+Input Format:
+The first line contains T.
+The second line contains X.
+
+Output Format:
+The first line contains the updated time T.
+
+Example Input/Output 1:
+
+Input:
+08:15
+20
+
+Output:
+08:35
+
+Explanation:
+Here, HH=08, MM-15 and
+After adding 20 minutes, the time T becomes 08:35
+
+So 08:35 printed as the output.
+
+
+Example Input/Output 2:
+
+Input:
+23:50
+15
+
+Output:
+00:05
+
+Max Execution Time Limit: 1000 millisecs
+
+"""
