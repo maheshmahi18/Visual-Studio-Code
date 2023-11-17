6e8404fda76f00166a2c55eb81a9a6ffc13449d8
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-11-17 04:07:19 +0000

    18/11/23 DT Question

---
+++ Daily Test/String Triangle Pattern.py
@@ -0,0 +1,47 @@
+"""
+
+String Triangle Pattern
+
+The program must accept a string S as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s):
+3 <= Length of S <= 100
+
+Input Format:
+The first line contains S.
+
+Output Format:
+The lines containing the desired pattern as shown in the Example Input/Output section.
+
+
+Example Input/Output 1:
+
+Input:
+skillrack
+
+Output:
+s********
+ks*******
+ski******
+liks*****
+skill****
+rlliks***
+skillra**
+carlliks*
+skillrack
+
+
+Example Input/Output 2:
+
+Input:
+hope
+
+Output:
+h***
+oh**
+hop*
+epoh
+
+
+Max Execution Time Limit: 500 millisecs
+"""
