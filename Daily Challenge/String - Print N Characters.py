7f4edc7466c9dd7be3a92f6004c184b9e93dd801
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-11-18 18:25:14 +0000

    18/11/23 DC Question

---
+++ Daily Challenge/String - Print N Characters.py
@@ -0,0 +1,44 @@
+"""
+
+String - Print N Characters
+
+The program must accept an integer N as the input The program must print N characters from the string "abcd" circularly in the clockwise direction as the output.
+
+Boundary Condition(s):
+1 <= N <= 10^5
+
+Input Format:
+The first line contains N.
+
+Output Format
+The first line contains a string denoting the N characters.
+
+Example Input/Output 1:
+
+Input
+5
+
+Output
+abcda
+
+Explanation:
+
+Here N=5.
+So 5 characters from the string "abcd" are printed circularly in the dockwise direction.
+
+Hence the output is abcda
+
+
+Example Input/Output 2:
+
+Input
+10
+
+Output
+abcdabcdab
+
+
+Max Execution Time Limit: 1000 millisecs
+
+
+"""
