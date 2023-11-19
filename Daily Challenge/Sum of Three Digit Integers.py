4b1ec06df85d4f07a5094269ed6d9d8609ef5ecd
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-11-19 11:26:44 +0000

    19/11/23 DC Question

---
+++ Daily Challenge/Sum of Three Digit Integers.py
@@ -0,0 +1,44 @@
+"""
+
+Sum of Three Digit Integers
+
+The program must accept an integer N (where the number of digits in N is always divisible by 3) as the input. The program
+must split the integer N into three-digit integers (every three consecutive digits). Then the program must print the sum S of
+those three-digit integers as the output.
+
+Boundary Condition(s):
+100 <= N <= 999999999
+
+Input Format:
+The first line contains N.
+
+Output Format:
+The first line contains S.
+
+Example Input/Output 1:
+
+Input:
+210045
+
+Output:
+255
+
+Explanation:
+Here N = 210045.
+The first three-digit integer of 210045 is 210.
+The last three-digit integer of 210045 is 045.
+
+So their sum 255 (210 + 045) is printed as the output.
+
+
+Example Input/Output 2:
+
+Input:
+100100
+
+Output:
+200
+
+
+Max Execution Time Limit: 1000 millisecs
+"""
