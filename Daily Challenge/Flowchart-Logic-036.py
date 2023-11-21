c6a3ba705c21efa7b61cfba91b0a9063b882112c
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-11-21 08:45:55 +0000

    21/11/23 DC Question

---
+++ Daily Challenge/Flowchart-Logic-036.py
@@ -0,0 +1,22 @@
+"""
+
+1. Start
+2. Declare two integer variables M and N
+3. Read the values of M and N
+4. Print the integer value returned by getNumberOfBinaryOnes(M^N)
+5. End
+
+Note: The symbol ^ refers the Bitwise XOR operator.
+
+6. getNumberOfBinaryOnes(integer N)
+7. Declare an integer variable onesCount and assign 0 to it
+
+8. Condition: Check if N is not equal to 0
+9.     Add N % 2 to onesCount
+10.    Replace N by N / 2
+11.    Go to step 8
+12. Return the value of onesCount
+
+Max Execution Time Limit: 1000 millisecs
+"""
+
