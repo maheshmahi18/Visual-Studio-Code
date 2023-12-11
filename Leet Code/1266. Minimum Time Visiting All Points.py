3453421fcb5879612063071c742eb76d82454bf2
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-12-11 06:18:13 +0000

    03/12/23 LCC Done

--- Leet Code/1266. Minimum Time Visiting All Points.py
+++ Leet Code/1266. Minimum Time Visiting All Points.py
@@ -45,3 +45,16 @@ points[i].length == 2
 Solution: Done in Python
 
 """
+
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        d=0
+        for i in range(len(points)-1):
+            a=abs(points[i][0]-points[i+1][0])
+            b=abs(points[i][1]-points[i+1][1])
+            d+=max(a,b)
+        return d
+
+sol=Solution()
+n=eval(input())
+print(sol.minTimeToVisitAllPoints(n))
