c398b2b75db964942d1eb6d3624b4004cd4d614d
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-12-11 06:33:15 +0000

    03/12/23 LCC Done

--- Leet Code/1160. Find Words That Can Be Formed by Characters.py
+++ Leet Code/1160. Find Words That Can Be Formed by Characters.py
@@ -10,7 +10,10 @@ Return the sum of lengths of all good strings in words.
 
 Example 1:
 
-Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Input: 
+words = ["cat","bt","hat","tree"]
+chars = "atach"
+
 Output: 6
 
 Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
@@ -18,7 +21,10 @@ Explanation: The strings that can be formed are "cat" and "hat" so the answer is
 
 Example 2:
 
-Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Input:
+words = ["hello","world","leetcode"]
+chars = "welldonehoneyr"
+
 Output: 10
 
 Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
@@ -34,3 +40,19 @@ words[i] and chars consist of lowercase English letters.
 Solution: Done in Python
 
 """
+
+class Solution(object):
+    def countCharacters(self, words, chars):
+        c=0
+        for i in words:
+            a=list(chars);b=1
+            for j in i:
+               if j in a: a.remove(j)
+               else: b=0;break
+            if b: c+=len(i)
+        return c
+    
+sol=Solution()
+a=eval(input())
+b=input()
+print(sol.countCharacters(a,b))
