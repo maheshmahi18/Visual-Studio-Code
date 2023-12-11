995ca0570f32196e46b4bdc9a5f7aa5caed5497a
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-12-11 05:56:53 +0000

    08/12/23 LCC Done

--- Leet Code/606. Construct String from Binary Tree.py
+++ Leet Code/606. Construct String from Binary Tree.py
@@ -30,3 +30,45 @@ The number of nodes in the tree is in the range [1, 104].
 Solution: Done in Python
 
 """
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+class Solution(object):
+  def tree2str(self, root):
+    if not root:
+      return ""
+    res = str(root.val)
+    if root.left or root.right:
+      res += "(" + self.tree2str(root.left) + ")"
+    if root.right:
+      res += "(" + self.tree2str(root.right) + ")"
+    return res
+
+def buildTreeFromList(lst):
+  if not lst:
+    return None
+  nodes = [None if val is None else TreeNode(val) for val in lst]
+  root = nodes[0]
+  queue = [root]
+  i = 1
+  while i < len(nodes):
+    current = queue.pop(0)
+    if nodes[i] is not None:
+      current.left = nodes[i]
+      queue.append(nodes[i])
+    i += 1
+    if i < len(nodes) and nodes[i] is not None:
+      current.right = nodes[i]
+      queue.append(nodes[i])
+    i += 1
+  return root
+
+
+sol = Solution()
+s = eval(input())
+root = buildTreeFromList(s)
+print(sol.tree2str(root))
