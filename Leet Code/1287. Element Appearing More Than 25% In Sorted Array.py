94a987e8eb6834b69a319c185bf2300b037d4682
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-12-11 04:00:35 +0000

    11/12/23 LCC

---
+++ Leet Code/1287. Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,28 @@
+"""
+
+1287. Element Appearing More Than 25% In Sorted Array
+
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+
+Example 2:
+
+Input: arr = [1,1]
+Output: 1
+ 
+Example 2:
+
+Input: arr = [1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]
+Output: 1
+
+
+Constraints:
+
+1 <= arr.length <= 10^4
+0 <= arr[i] <= 10^5
+
+"""
