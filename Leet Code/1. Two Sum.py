ecc1040bf062175c690fdf685dd3948c8276e67d
Mahesh Gorla <105862877+maheshmahi18@users.noreply.github.com>
2023-12-11 06:46:06 +0000

    13/09/23 LCC Done

--- Leet Code/1. Two Sum.py
+++ Leet Code/1. Two Sum.py
@@ -10,7 +10,10 @@ You can return the answer in any order.
 
 Example 1:
 
-Input: nums = [2,7,11,15], target = 9
+Input: 
+nums = [2,7,11,15]
+target = 9
+
 Output: [0,1]
 
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
@@ -18,13 +21,19 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 Example 2:
 
-Input: nums = [3,2,4], target = 6
+Input: 
+nums = [3,2,4]
+target = 6
+
 Output: [1,2]
 
 
 Example 3:
 
-Input: nums = [3,3], target = 6
+Input: 
+nums = [3,3]
+target = 6
+
 Output: [0,1]
  
 
@@ -42,3 +51,16 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
 Solution: Done in Python
 
 """
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j]==target):
+                    return ([i,j])
+                    break
+
+sol=Solution()
+nums=eval(input())
+target=int(input())
+print(sol.twoSum(nums,target))
